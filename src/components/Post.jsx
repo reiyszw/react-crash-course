b0c98e9bce990d2828b5f5a99790a669ff925b71
reiyszw <reiyszw@gmail.com>
2026-07-01 08:55:49 +0900

    Passing Data to Components with Props

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,8 @@
-const names = ["Maximilian", "Manuel"];
-
-function Post() {
-  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
-
+function Post({ author, body }) {
   return (
     <div>
-      <p>{chosenName}</p>
-      <p>React.js is awesome!</p>
+      <p>{author}</p>
+      <p>{body}</p>
     </div>
   );
 }
